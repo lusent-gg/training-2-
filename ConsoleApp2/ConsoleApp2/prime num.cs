6c00f73d74c7212f053b532a189a44408338fe1d
lusent-gg <arshyganhmhr@gmail.com>
2024-11-02 20:03:26 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/prime num.cs
@@ -0,0 +1,36 @@
+﻿
+Console.Write("adad KOCHIK ra vared konid :");
+int num = int.Parse(Console.ReadLine());
+
+
+Console.Write("adad bozorg ra vared konid :");
+int num2 = int.Parse(Console.ReadLine());
+
+while ( num <= num2 )
+{
+    for (int i = 2; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            if (num != i)
+                break;
+            else if (num == i)
+                Console.WriteLine(num + "is prime");
+        }
+            
+
+    }
+    num++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
