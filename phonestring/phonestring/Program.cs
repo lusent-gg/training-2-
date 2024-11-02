35c7f617db943a7bacbb6e5e3ed298e9537b88bc
arash yeganeh mehr <arshyganhmhr@gmail.com>
2024-11-02 23:01:23 +0300

    Add files via upload

---
+++ phonestring/phonestring/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+
+
+Console.Write("jomle ra vared konid :");
+string phone = Console.ReadLine();
+
+
+string check = "";
+
+
+for(int i = 0; i < phone.Length; i++)
+{
+   
+
+
+    if (phone[i] == '0')
+    {
+        for(int u = i  ; u < i + 10 ; u++)
+        {
+            check += phone[u]; 
+            
+            if(check.Length == 10)
+            {
+                Console.WriteLine(check);
+                check = "";
+                break;
+            }
+
+
+
+        }
+
+    }
+    
+}
